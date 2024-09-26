c996519089e2c276ed1c44ca5a7b14386a64ad33
Emanuele Messina <emanuelemessina.em@gmail.com>
2024-09-27 01:58:01 +0200

    code refactor into files

--- src/props.py
+++ src/props.py
@@ -1,8 +1,9 @@
 import bpy
+from . import cylinder
 
 class Properties(bpy.types.PropertyGroup):
-    radius: bpy.props.FloatProperty(name="Radius", default=1.0, min=0.01, update=lambda self, context: update_cylinder(self, context))
-    depth: bpy.props.FloatProperty(name="Depth", default=2.0, min=0.01, update=lambda self, context: update_cylinder(self, context))
-    vertices: bpy.props.IntProperty(name="Vertices", default=32, min=3, max=256, update=lambda self, context: update_cylinder(self, context))
+    radius: bpy.props.FloatProperty(name="Radius", default=1.0, min=0.01, update=lambda self, context: cylinder.update(self, context)) # type: ignore
+    depth: bpy.props.FloatProperty(name="Depth", default=2.0, min=0.01, update=lambda self, context: cylinder.update(self, context)) # type: ignore
+    vertices: bpy.props.IntProperty(name="Vertices", default=32, min=3, max=256, update=lambda self, context: cylinder.update(self, context)) # type: ignore
 
 classes = [Properties]
